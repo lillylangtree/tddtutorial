1e238062da30b24db18a77b269f0d19ec11be092
Sean Cahill <projects@nmm.ie>
2015-02-19 13:27:29 +0000

    first commit

---
+++ public/javascripts/directives.js
@@ -0,0 +1,4 @@
+/**
+ * Created by sean on 19/02/2015.
+ */
+var appDirectives = angular.module('myDirectives', []);
---
+++ public/javascripts/services.js
@@ -0,0 +1,4 @@
+/**
+ * Created by sean on 19/02/2015.
+ */
+var appServices = angular.module('myServices', ['ngResource']);
